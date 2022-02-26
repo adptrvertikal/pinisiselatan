bcb60ad074456cad6b0eb7997cfaa7ea53d875a0
adptrvertikal <94295905+adptrvertikal@users.noreply.github.com>
2022-02-26 07:44:01 +0700

    server baru

---
+++ README.md
@@ -0,0 +1,32 @@
+# vuejs-portofolio
+
+> Portfolio template using VueJs framework.
+
+![Vue](https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D) ![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white) ![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)![Netlify](https://img.shields.io/badge/Netlify-00C7B7?style=for-the-badge&logo=netlify&logoColor=white) ![jQuery](https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white)
+
+
+## Features
+
+- Builded with VueJs framework ✨
+- One page layout ✨
+- Material design ✨
+- Bootstrap 4.5 & css ✨
+- Responsive ✨
+- Animated layout ✨
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+# serve with hot reload at localhost:8080
+npm run dev
+# build for production with minification
+npm run build
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## 📝 License
+
+This project is under [MIT] license
---
+++ info.js
@@ -0,0 +1,256 @@
+let info = {
+    name: "Muhamad Jaya Adiputera",
+    logo_name: "Adiputera",
+    flat_picture: require("./src/assets/potrait.jpg"),
+    config: {
+        use_cookies: true,
+        navbar: {
+            blur: false
+        }
+    },
+    description: "Saya Muhamad Jaya Adiputera, ini adalah Final Project Saya saat mengikuti Bootcamp Fullstack developer ",
+    links: {
+        linkedin: "linkedin.com/in/muhamad-jaya-adiputera",
+        github: "https://github.com/adptrvertikal",
+        behance: "https://www.behance.net/muhammaadipute",
+        resume: "./src/assets/pdfs/my resume 2021.pdf"
+    },
+    education: [{
+        name: "Politeknik Negeri Sriwidjaja",
+        place: "Palembang, Sumatera Selatan Indonesia",
+        date: "Sep, 2006 - 2009",
+        degree: "Diploma Tourism and Hospitality",
+        gpa: "3.33/4.0",
+        description: "",
+        skills: [
+            "Food and Beverages service and Product",
+            "Auditing",
+            "UI/UX design",
+            "Front-end developing",
+            "C#"
+        ]
+    }, ],
+    experience: [{
+            name: "Kejaksaan Agung Republik Indonesia",
+            place: "Jakarta, Indonesia",
+            date: "March, 2019 - currently",
+            position: "Honor Teknis Bagian Keuangan, Direktorat Penyidikan",
+            description: "",
+            skills: ["MsOffice", "SAKTIapp", "Manajemen Resiko"]
+        },
+        {
+            name: "The Alts Hotel Palembang",
+            place: "Palembang, Indonesia",
+            date: "1 Jan, 2017 - 15, Feb, 2019",
+            position: "Night Auditor",
+            description: "",
+            skills: ["Powerpro FO", "PowerProFnB", "PowerProAudit"]
+        },
+        {
+            name: "PT Bank Rakyat Indonesia",
+            place: "Jambi, Indonesia",
+            date: "jun, 2013 - sep, 2017",
+            position: "Mantri KUR, Kepala Teras BRI",
+            description: "",
+            skills: ["Loan Approval System App", "Manajemen Resiko", "Forecast", "Begadang"]
+        },
+        {
+            name: "PT Bank Mandiri (persero) Tbk",
+            place: "{Palembang, Indonesia",
+            date: "Jan, 2009 - Feb, 2013",
+            position: "Mikro Kredit Sales",
+            description: "",
+            skills: ["Rush Approach", "Begadang", "Verifikasi"]
+        },
+    ],
+    skills: [{
+            title: "Language",
+            info: [
+                "Python", "Javascript", "TypeScript", "Java"
+            ],
+            icon: "fa fa-code"
+        },
+        {
+            title: "Front-end",
+            info: [
+                "HTML5", "CSS4", "Bootstrap5", "JQuery", "VUejs2", "json"
+            ],
+            icon: "fa fa-cubes"
+        },
+        {
+            title: "web technologies",
+            info: ["Vue", "React", "Node"],
+            icon: "fas fa-laptop-code"
+        },
+        {
+            title: "Databases",
+            info: ["MongoDB", "MySQL", "SQlyog"],
+            icon: "fa fa-database"
+        },
+        {
+            title: "Operating systems & tools",
+            info: ["Ubuntu", "Windows"],
+            icon: "fas fa-tools"
+        },
+        {
+            title: "Design",
+            info: ["Illustrator", "FIGMA", "Photoshop"],
+            icon: "fa fa-pencil-square-o"
+        }
+    ],
+    portfolio: [{
+            name: "Login Page",
+            pictures: [{
+                    img: require("./src/assets/portfolio/diary/1.png")
+                },
+                {
+                    img: require("./src/assets/portfolio/diary/2.png")
+                },
+                {
+                    img: require("./src/assets/portfolio/diary/3.png")
+                },
+            ],
+            technologies: ["Javascript", "CSS"],
+            category: "Web App",
+            date: "Aug, 2021 - 20 days",
+            github: "https://github.com/",
+            visit: "https://github.com/",
+            description: " contoh Halaman Web responsive dengan CSS, Bootstrap dan Javascript"
+        },
+        {
+            name: "Calculator",
+            pictures: [{
+                    img: require("./src/assets/portfolio/online examination/exam (1).png")
+                },
+                {
+                    img: require("./src/assets/portfolio/online examination/exam (2).png")
+                },
+                {
+                    img: require("./src/assets/portfolio/online examination/exam (3).png")
+                },
+                {
+                    img: require("./src/assets/portfolio/online examination/exam (4).png")
+                },
+                {
+                    img: require("./src/assets/portfolio/online examination/exam (5).png")
+                }
+            ],
+            technologies: ["HTML", "CSS", "JavaSript", "Bootstrap"],
+            category: "Web App",
+            date: "Apr, 2021 - May, 2021",
+            github: "#",
+            visit: "#",
+            description: "App Kalkulator sederhana "
+        },
+        {
+            name: "Portofolio Web",
+            pictures: [{
+                    img: require("./src/assets/portfolio/myflix.com/1.png")
+                },
+                {
+                    img: require("./src/assets/portfolio/myflix.com/2.png")
+                },
+                {
+                    img: require("./src/assets/portfolio/myflix.com/3.png")
+                },
+                {
+                    img: require("./src/assets/portfolio/myflix.com/4.png")
+                },
+            ],
+            technologies: ["Javascript", "css", "HTML", "webpack", "Bootstrap", "Jquery"],
+            category: "Web App",
+            date: "Aug, 2021 - 10 days",
+            github: "#",
+            visit: "#",
+            description: " web portofolio projrct pertama. "
+        },
+    ],
+    portfolio_design: [{
+            name: "First design ",
+            title: "Perdana design",
+            pictures: [{
+                    img: require("./src/assets/designs/online examination/exam (2).png"),
+                    title: "MoodBoard"
+                },
+                {
+                    img: require("./src/assets/designs/online examination/exam (1).png"),
+                    title: "Mockups 1"
+                },
+                {
+                    img: require("./src/assets/designs/online examination/exam (3).png"),
+                    title: "Mockups 2"
+                },
+                {
+                    img: require("./src/assets/designs/online examination/exam (4).png"),
+                    title: "App Elements"
+                },
+                {
+                    img: require("./src/assets/designs/online examination/exam (5).png"),
+                    title: "App Elements"
+                },
+                {
+                    img: require("./src/assets/designs/online examination/exam (6).png"),
+                    title: "App Elements"
+                }
+            ],
+            technologies: ["FIGMA"],
+            category: "Visual Design",
+            github: "",
+            date: "Jan, 2021 - Feb, 2021",
+            visit: "",
+            description: "<br> <br> with Ui/UX design and full stack developing(HTML/CSS/JQuery/PHP)."
+        },
+        {
+            name: "insight",
+            title: "insight - Mockup Design",
+            pictures: [{
+                    img: require("./src/assets/designs/pill reminder/web 1.png"),
+                    title: "Moodboard"
+                },
+                {
+                    img: require("./src/assets/designs/pill reminder/1.png"),
+                    title: "Mockups 1"
+                },
+                {
+                    img: require("./src/assets/designs/pill reminder/2.png"),
+                    title: "Mockups 2"
+                }
+            ],
+            technologies: ["XD", "Illustrator"],
+            category: "Visual Design",
+            github: "",
+            date: "Apr, 2021 - Apr, 2021",
+            visit: "",
+            description: "Design poster vertikal."
+        },
+        {
+            name: "Taxes Burger",
+            title: "Taxes Burger - UI design",
+            pictures: [{
+                    img: require("./src/assets/designs/burger/Web 1 (1).jpg"),
+                    title: "Moodboard"
+                },
+                {
+                    img: require("./src/assets/designs/burger/Web 1 (1).png"),
+                    title: "Mockup 1"
+                },
+                {
+                    img: require("./src/assets/designs/burger/Web 1 (2).png"),
+                    title: "Mockup 2"
+                },
+                {
+                    img: require("./src/assets/designs/burger/Web 1 (3).png"),
+                    title: "Mockup 3"
+                },
+            ],
+            technologies: ["XD", "Illustrator"],
+            category: "Visual Design",
+            github: "",
+            date: "May, 2021 - May, 2021",
+            visit: "",
+            description: " Contoh Burger restaurant UI design"
+        }
+    ],
+};
+
+export default info;
